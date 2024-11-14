bd270e2232726b2380c1667dbd8fdcd714fcff37
Angelica Lopes <141454840+angelicalopes2006@users.noreply.github.com>
2024-11-14 08:27:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    "Geografia",
+    "Quais os menores e os maiores países do mundo?",
+    "Vaticano e Rússia",
+)
+criaCartao(
+    "Matemática",
+    "quanto é 5 x 5?",
+    "25",
+)
+criaCartao(
+    "Química",
+    "Qual a substância que compõe a água?",
+    "H20",
+)
+criaCartao(
+    "Português",
+    "Quem escreveu Dom Casmurro?",
+    "Machado de Assis",
+)
+criaCartao(
+    "Espanhol",
+    "O que significa vaso?",
+    "Copo",
+)
+criaCartao(
+    "Inglês",
+    "Como se pronuncia professora?",
+    "Teacher",
+)
